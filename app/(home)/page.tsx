a59506f855e250831114ebf2dcbfcc6cd7cafbc4
Zoltan Busi <oma.spencer@gmail.com>
2023-12-28 10:52:37 +0100

    Add .env file and cleanup Home and create SearchPage route

---
+++ app/(home)/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <h1>Welcome on SWAPI API with NextJS project.</h1>
+    </main>
+  );
+};
+
+export default Home;
